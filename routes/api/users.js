4a4a51e010c8b27f31c52cbfbf65293ad617d628
Srikant Kalaputapu <skalaputapu@gmail.com>
2020-10-11 17:14:35 -0400

    Added post route for creating user, added express middleware (used to be bodyparser) for parsing json data

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-// @route   GET api/users
-// @desc    Test route
+// // @route   GET api/users
+// // @desc    Test route
+// // @access  Public
+// router.get('/', (req, res) => res.send('User Route'));
+
+// create a user. Needs middleware
+// @route   POST api/users
+// @desc    Register user
 // @access  Public
-router.get('/', (req, res) => res.send('User Route'));
-
-
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('User Route');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 5000;
 // connect to db
 connectDB();
 
+// init middleware for the response/request data in json
+// used to be bodyparser, now part of express
+app.use(express.json({extended: false}));
 
 app.get('/', (req, res) => res.send('API Running'));
 
