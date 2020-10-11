e208b802a9f026ef2cf9e0ce80803cbc954f2117
Srikant Kalaputapu <skalaputapu@gmail.com>
2020-10-11 17:42:46 -0400

    added functionality for creating user with encrypted password and gravatar

--- config/db.js
+++ config/db.js
@@ -1,24 +1,25 @@
 // Mongo connection
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 // people use async await now
 // async Promise
 // mongoose.connect(db)
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, { 
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("MongoDB connected");
-    } catch(err){
-        console.error(err.message);
-        // Exit process with failure
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,10 +3,17 @@ const router = express.Router();
 //validation. adds another parameter to router commands
 const { check, validationResult } = require("express-validator");
 
+const gravatar = require("gravatar");
+
+const bcrypt = require("bcryptjs");
+
 // // @route   GET api/users
 // // @desc    Test route
 // // @access  Public
 // router.get('/', (req, res) => res.send('User Route'));
+
+// Get model for mongoose queries
+const User = require("../../models/User");
 
 // create a user. Needs middleware
 // @route   POST api/users
@@ -22,11 +29,53 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     // validation
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // see if the user exists - respond with error if already exists
+      // parameter is email to this just works.
+      // wait until we see if email already in use
+      let user = await User.findOne({ email });
+
+      if (user) {
+        // mirror the validation array so that on the front end everything is consistent
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // get users gravatar based on email
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      // create user (doesnt save)
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt password using BCrypt. Instead of .then use awaits
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      // Return the jsonwebtoken - user logs in when it is created. need token
+      res.send("User registered");
+    } catch (err) {
+      console.log(err.message);
+      return res.status(500).send("Server Error");
     }
 
     console.log(req.body);
